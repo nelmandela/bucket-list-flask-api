c8f8599fb92c6f782e8e9cb0785a7a5966bf6fb3
Arnold Okoth <arnold.okoth@andela.com>
2017-07-12 17:32:00 +0300

    [CHORE] add config file

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+import os
+import sys
+from flask import Flask
+# Add top-level directory to module search path.
+current_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_path)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '123456789'
