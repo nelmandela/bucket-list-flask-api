4ed38cf2717f627048264a4326bb93cf712a83f7
Arnold Okoth <arnold.okoth@andela.com>
2017-07-12 17:28:25 +0300

    [FEATURE] add models

---
+++ app/models/models.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bucketlist.db'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    username = db.Column(db.String(128))
+    password = db.Column(db.String(128))
+    email = db.Column(db.String(128))
+
+class Bucketlist(db.Model):
+    bucket_id = db.Column(db.Integer, primary_key=True)
+    bucket_name = db.Column(db.String)
+    bucket_descriptions = db.Column(db.String)
