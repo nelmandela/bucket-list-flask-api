21aa68108e6ee66d575caadaa8220256128f6441
Arnold Okoth <arnold.okoth@andela.com>
2017-07-14 18:01:06 +0300

    [CHORE] manage migrations

--- manage.py
+++ manage.py
@@ -2,16 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app.models.models import app
 from app.models.models import db
 from app.models.models import migrate
 from app.models.models import User
 from app.models.models import Bucketlist
-# app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bucketlist.db'
-
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
+from app import app
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
