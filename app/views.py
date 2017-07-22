4000673a4a2b0f487c845a03486a8a5cc006ec70
josiah nyarega <josiah.mokobo@andela.com>
2017-07-22 17:39:49 +0300

    [CHORE] refactore views.py for the restiful api

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
-from flask import render_template, request, Flask, jsonify, make_response
+from flask import render_template, request, Flask, jsonify, make_response, Blueprint
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
-import jwt
+from flask_jwt import JWT, jwt_required
 import datetime
 from functools import wraps
+from flask_restful_swagger import swagger
+from flask_restful import reqparse, abort, Api, Resource
 
 from app import app
 from app.models.user_controller import UserStore
@@ -13,196 +15,186 @@ from app.models.items_controller import ItemStore
 
 user = UserStore()
 bucket = BucketStore()
-item  = ItemStore()
+item = ItemStore()
 
-@app.route('/')
-def index():
-    '''
-    Route enables a user to register
-    '''
-    return ""
+app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'super-secret'
 
-@app.route('/auth/register', methods=['POST'])
-def register():
-    '''
-    Route enables a user to register
-    '''
-    data = request.get_json()
-    username = data['username']
-    password = generate_password_hash(data['password'])
-    name    = data['name']
-    email   = data['email']
-    response = user.create_user(username=username, password_hash=password, name=name, email=email, public_id=str(uuid.uuid4()))
-    return jsonify({"response": response})
+api = Api(app)
+api = swagger.docs(Api(app), apiVersion='0.1')
 
-@app.route('/auth/login', methods=['POST'])
-def login():
-    auth = request.authorization
-    if not auth or not auth.username or not auth.password:
-        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
-    user_response = user.login(auth.username)
-    if not user_response:
-        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
-    if check_password_hash(user_response.password, auth.password):
-        token = jwt.encode({'public_id' : user_response.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
-        return jsonify({'token' : token.decode('UTF-8')})
-    return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
-
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-        print(kwargs)
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-
-        if not token:
-            return jsonify({'message' : 'Token is missing!'}), 401
-
-        try: 
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-            current_user = user.get_current_user(data['public_id'])
-            
-        except:
-            return jsonify({'message' : 'Token is invalid!'}), 401
-
-        return f(current_user, *args, **kwargs)
-
-    return decorated
-
-@app.route('/bucketlists', methods=['POST'])
-@token_required
-def bucketlists(current_user):
-    '''
-    Route enables users to add their own bucketlist
-    '''
-    data = request.get_json()
-    bucket_name = data['bucket_name']
-    bucket_description   = data['bucket_description']
-    user_id     = data['user_id']
-    response = bucket.create_bucketlist(bucket_name=bucket_name, bucket_description=bucket_description, user_id=user_id)
-    return jsonify({"response": response})
-
-@app.route('/bucketlists/<int:bucket_id>', methods=['GET'])
-@token_required
-def view_bucketlists(current_user,bucket_id):
-    '''
-    Route enables users to view thier bucketlists by id
-    '''
-    response = bucket.get_bucket(bucket_id)
-    print(response)
-    return jsonify({"response": response})
-
-@app.route('/bucketlists')
-@app.route('/bucketlists/<limit>/<q>', methods=['GET'])
-@token_required
-def all_bucketlists(current_user, limit=None, q=None):
-    '''
-    Route enables users to view all thier bucketlists
-    '''
-    limit_value  = request.args.get('limit')
-    q = request.args.get('q')
-    response = bucket.get_all(limit_value, q)
-    return jsonify({"response": response})
-
-@app.route('/bucketlists/<int:bucket_id>', methods=['PUT'])
-@token_required
-def update_bucketlists(current_user,bucket_id):
-    '''
-    Route enables users to update thier bucketlists
-    '''
-    data = request.get_json()
-    response = bucket.update(bucket_id=bucket_id, bucket_name=data['bucket_name'], bucket_description=data['bucket_description'])
-    updated_response = bucket.get_bucket(bucket_id)
-    response_information = ''
-    if response == True:
-        response_information = updated_response
-    else:
-        response_information = 'Bucketlist doesnt exist'
-    return jsonify({"response":response_information})
-
-@app.route('/bucketlists/<int:bucket_id>/', methods=['DELETE'])
-@token_required
-def delete_bucketlists(current_user, bucket_id):
-    '''
-    Route enables users to delete thier bucketlists by id
-    '''
-    updated_response = bucket.delete(bucket_id)
-    return jsonify({"response": updated_response})
-
-@app.route('/bucketlists/<int:bucket_id>/items', methods=['POST'])
-@token_required
-def items_add(current_user, bucket_id):
-    '''
-    Route enables users to add thier bucketlists items
-    '''
-    data        = request.get_json()
-    item_name   = data['item_name']
-    item_status = data['item_status']
-    due_date    = data['due_date']
-    response = item.create_bucketlistitem(item_name=item_name, item_status=item_status, due_date=due_date, bucket_id=bucket_id)
-    return jsonify({"response": response})
+USER_DATA = {
+    "masnun": "abc123"
+}
 
 
-@app.route('/bucketlists/<int:bucket_id>/items', methods=['GET'])
-@app.route('/bucketlists/<int:bucket_id>/items/<limit>/<q>', methods=['GET'])
-@token_required
-def get_all_items(current_user, bucket_id):
-    '''
-    Route enables users to view thier bucketlists items
-    '''
-    limit_value  = request.args.get('limit')
-    q = request.args.get('q')
-    print(limit_value)
-    response = item.get_all(bucket_id, limit_value, q)
-    return jsonify({"response": response})
+class User(object):
+    def __init__(self, id):
+        self.id = id
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
 
 
-@app.route('/bucketlists/<int:bucket_id>/items/<int:item_id>', methods=['GET'])
-@token_required
-def get_items_by_id(current_user, bucket_id, item_id):
-    '''
-    Route enables users to view thier bucketlists items by id
-    '''
-    response = item.get_item_by_id(bucket_id, item_id)
-    print(response)
-    return jsonify({"response": response})
+def verify(username, password):
+    if not (username and password):
+        return False
+    if USER_DATA.get(username) == password:
+        return User(id=123)
 
-@app.route('/bucketlists/<int:bucket_id>/items/<int:item_id>', methods=['PUT'])
-@token_required
-def update_item(current_user, bucket_id, item_id):
-    '''
-    Route enables users to update thier bucketlists items
-    '''
-    data = request.get_json()
-    item_name = data['item_name']
-    item_status = data['item_status']
-    due_date = data['due_date']
-    response = item.update_item(bucket_id=bucket_id, item_id=item_id, item_name=item_name, item_status=item_status, due_date=due_date) 
-    print(response)
-    return jsonify({"response": response})
 
-@app.route('/bucketlists/<int:bucket_id>/items/<int:item_id>', methods=['DELETE'])
-@token_required
-def delete_item(current_user, bucket_id, item_id):
-    '''
-    Route enables users to delete thier bucketlists items
-    '''
-    response = item.delete_item(bucket_id, item_id)
-    print(response)
-    return jsonify({"response": response})
+def identity(payload):
+    user_id = payload['identity']
+    return {"user_id": user_id}
 
-@app.route('/allitems/', methods=['GET'])
-@token_required
-def all_items(current_user):
-    response = item.get_items()
-    print(response)
-    return jsonify({"response": response})
 
-@app.route('/users/', methods=['GET'])
-@token_required
-def all_users(current_user):
-    response = user.get_all()
-    for res in response:
-        print(res.password)
+jwt = JWT(app, verify, identity)
+
+
+class Bucketlist(Resource):
+    @swagger.operation(
+        notes='delete a todo item by ID',
+    )
+    @jwt_required()
+    def get(self, **kwargs):
+        limit = request.args.get('limit')
+        search = request.args.get('q')
+        if limit:
+            response = bucket.get_all_buckets_with_limit(
+                limit)
+            return response
+
+        elif search:
+            response = bucket.get_all_buckets_with_limit_query(search, limit)
+            return response
+
+        elif kwargs['bucket_id']:
+            response = bucket.get_bucket(kwargs['bucket_id'])
+            return jsonify({"response": response})
+
+    @swagger.operation(
+        notes='delete a todo item by bucket_id',
+    )
+    @jwt_required()
+    def delete(self, **kwargs):
+        response = bucket.delete(kwargs['bucket_id'])
+        return response
+
+    @swagger.operation(
+        notes='delete a todo item by ID',
+    )
+    @jwt_required()
+    def post(self, **kwargs):
+        bucket_name = "mokobo"
+        bucket_description = "nyarega"
+        user_id = 1
+        response = bucket.create_bucketlist(
+            bucket_name=bucket_name, bucket_description=bucket_description, user_id=user_id)
+        return jsonify({"response": response})
+
+    @swagger.operation(
+        notes='delete a todo item by ID',
+    )
+    @jwt_required()
+    def put(self, bucket_id):
+        response = bucket.update(
+            bucket_id=bucket_id, bucket_name='james', bucket_description='Mathew')
+        updated_response = bucket.get_bucket(bucket_id)
+        response_information = ''
+        if response is True:
+            response_information = updated_response
+        else:
+            response_information = 'Bucketlist doesnt exist'
+        return jsonify({"response": response_information})
+
+
+class BucketlistItems(Resource):
+    @swagger.operation(
+        notes='delete a todo item by ID',
+    )
+    @jwt_required()
+    def get(self, **kwargs):
+        limit = request.args.get('limit')
+        q = request.args.get('q')
+        response = None
+        if q and limit:
+            response = item.get_all_buckets_with_search_limit(
+                kwargs['bucket_id'], limit, q)
+        elif limit:
+            response = item.get_all_buckets_with_limit(
+                kwargs['bucket_id'], limit)
+        elif kwargs['item_id'] and kwargs['bucket_id']:
+            response = item.get_item_by_id(
+                kwargs['bucket_id'], kwargs['item_id'])
+        return jsonify({"response": response})
+
+    @swagger.operation(
+        notes='delete a todo item by ID',
+    )
+    @jwt_required()
+    def post(self, bucket_id):
+        data = request.get_json()
+        item_name = data['item_name']
+        item_status = data['item_status']
+        due_date = data['due_date']
+        response = item.create_bucketlistitem(
+            item_name=item_name, item_status=item_status, due_date=due_date, bucket_id=bucket_id)
+        return jsonify({"response": response})
+
+    @swagger.operation(
+        notes='delete a todo item by ID',
+    )
+    @jwt_required()
+    def delete(self, bucket_id, item_id):
+        response = item.delete_item(bucket_id, item_id)
+        return jsonify({"response": response})
+
+    @swagger.operation(
+        notes='delete a todo item by ID',
+    )
+    @jwt_required()
+    def put(self, bucket_id, item_id):
+        data = request.get_json()
+        item_name = data['item_name']
+        item_status = data['item_status']
+        due_date = data['due_date']
+        response = item.update_item(
+            item_name=item_name, item_status=item_status, due_date=due_date, bucket_id=bucket_id, item_id=item_id)
+        return jsonify({"response": response})
+
+
+class Users(Resource):
+
+    @jwt_required()
+    def get(self, **kwargs):
+        response = user.get_all()
+        return jsonify({"response": response})
+
+    def post(self, **kwargs):
+        data = request.get_json()
+        name = data["name"]
+        username = data["username"]
+        email = data["email"]
+        password_hash = data["password_hash"]
+        public_id = "12345"
+        response = user.create_user(
+            name=name, username=username, email=email, password_hash=password_hash, public_id=public_id)
+        return jsonify({"response": response})
+
+
+##
+# Endpoints
+##
+api.add_resource(BucketlistItems, '/api/v01/bucketlists/<int:bucket_id>/items/<int:item_id>/',
+                 '/api/v01/bucketlists/<int:bucket_id>/items/', endpoint='items')
+
+api.add_resource(Bucketlist, '/api/v01/bucketlists/<int:bucket_id>/',
+                 '/api/v01/bucketlists/', endpoint='bucketlists')
+
+api.add_resource(Users, '/api/v01/user/<int:user_id>/',
+                 '/api/v01/user/', endpoint='user')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
