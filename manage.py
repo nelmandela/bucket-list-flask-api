b4cff8931c51f4b73cc4bcb7ac5386aae21c44cd
josiah nyarega <josiah.mokobo@andela.com>
2017-08-11 10:51:25 +0300

    [FIX] Edit manage.py to run the application

--- manage.py
+++ manage.py
@@ -1,15 +1,17 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
 from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-from app.models.models import db
-from app.models.models import migrate
-from app.models.models import User
-from app.models.models import Bucketlist
-from app import app
+from flask_migrate import  MigrateCommand
+from app.view import *
+
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def runserver():
+    """Runs the unit tests without test coverage."""
+    app.run(debug=True)
 
 
 if __name__ == '__main__':
