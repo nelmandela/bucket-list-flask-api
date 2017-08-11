990c0eac65236d397767a31453d4d4221f2827b4
josiah nyarega <josiah.mokobo@andela.com>
2017-08-11 11:14:47 +0300

    [FIX] Edit config to  setup heroku database

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     SECRET_KEY = 'josiah'
-    SQLALCHEMY_DATABASE_URI = 'postgresql:///bucket'
+    SQLALCHEMY_DATABASE_URI = 'postgres://izksnosqnrihho:887702a798d6186fcb46cf8d562e76985454d3eec5418530f575deada78b4aec@ec2-107-20-250-195.compute-1.amazonaws.com:5432/d60omno6u66nm5'
     JWT_AUTH_URL_RULE = '/auth/login'
 
 
@@ -13,4 +13,4 @@ class TestingConfig(Config):
     SECRET_KEY = 'josiah'
     TESTING = True
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql:///bucket_test'
+    SQLALCHEMY_DATABASE_URI = 'postgres://izksnosqnrihho:887702a798d6186fcb46cf8d562e76985454d3eec5418530f575deada78b4aec@ec2-107-20-250-195.compute-1.amazonaws.com:5432/d60omno6u66nm5'
