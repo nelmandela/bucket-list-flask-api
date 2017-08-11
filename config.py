38afb820ef01e2216281b6675029ad661c59b582
josiah nyarega <josiah.mokobo@andela.com>
2017-08-11 11:58:47 +0300

    initial

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
 
 
 class DevelopmentConfig(Config):
     SECRET_KEY = 'josiah'
-    SQLALCHEMY_DATABASE_URI = 'postgres://izksnosqnrihho:887702a798d6186fcb46cf8d562e76985454d3eec5418530f575deada78b4aec@ec2-107-20-250-195.compute-1.amazonaws.com:5432/d60omno6u66nm5'
+    SQLALCHEMY_DATABASE_URI = 'postgresql:///bucket'
     JWT_AUTH_URL_RULE = '/auth/login'
 
 
@@ -13,4 +14,4 @@ class TestingConfig(Config):
     SECRET_KEY = 'josiah'
     TESTING = True
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgres://izksnosqnrihho:887702a798d6186fcb46cf8d562e76985454d3eec5418530f575deada78b4aec@ec2-107-20-250-195.compute-1.amazonaws.com:5432/d60omno6u66nm5'
+    SQLALCHEMY_DATABASE_URI = 'postgresql:///bucket_test'
