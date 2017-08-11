ea069598e216ca872a03d8b0343f4bc77a27c054
josiah nyarega <josiah.mokobo@andela.com>
2017-08-11 10:47:52 +0300

    [FIX] Edit __init__.py to configure development envirnment

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,10 @@
 import os
 import sys
-import config
 
 from flask import Flask
 # Add top-level directory to module search path.
 current_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(current_path)
-app = Flask(__name__)
-app.config['SECRET_KEY'] = config.SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URI
+app = Flask(__name__)   
+app.config.from_object('config.DevelopmentConfig')
 
-
-class Config(object):
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-class DevelopmentConfig(Config):
-    SECRET_KEY = 'asdfh87sf454yhggfd45dererfds22as112'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/bcktlst'
-    JWT_AUTH_URL_RULE = '/api/v1/auth/login'
-
-
-class TestingConfig(Config):
-    SECRET_KEY = '8h87yhggfd45dfds22as'
-    TESTING = True
-    WTF_CSRF_ENABLED = False
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://'\
-        'localhost/db_for_api_tests'
