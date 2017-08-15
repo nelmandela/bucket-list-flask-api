7680ddbba16dc97dd38e78d364ebf2449f5e96d9
josiah nyarega <josiah.mokobo@andela.com>
2017-08-15 14:31:27 +0300

    [CHORE] Edit __init__.py to allow cross_origin request

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 import os
 import sys
-
+from flask_cors import CORS, cross_origin
 from flask import Flask
 # Add top-level directory to module search path.
 current_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(current_path)
-app = Flask(__name__)   
+app = Flask(__name__)
+CORS(app)
 app.config.from_object('config.DevelopmentConfig')
-
