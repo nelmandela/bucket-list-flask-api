36c628bbd31fecb1a06a2bbb65b0153fb22e509c
josiah nyarega <josiah.mokobo@andela.com>
2017-08-21 10:28:50 +0300

    [BUG] Add status codes to responses

--- app/models/user_controller.py
+++ app/models/user_controller.py
@@ -1,4 +1,4 @@
-
+from flask import Flask, make_response, jsonify
 from werkzeug.security import check_password_hash
 
 from app.models.models import User
@@ -13,12 +13,13 @@ class UserStore(object):
 
     def create_user(self, **kwargs):
         ''' creates a new user '''
-        dictionary = {}
+        response = ""
         # validate fields
         print('' in kwargs.values())
         try:
             if '' in kwargs.values():
-                dictionary['response'] = 'All fields are required'
+                return make_response(jsonify(
+                    {"message": "All fields are required"}), 400)
             else:
                 # check if email or username already exists
                 match =\
@@ -26,28 +27,25 @@ class UserStore(object):
                         '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$',
                         kwargs['email'])
                 if match is None:
-                    dictionary['status_code'] = 400
-                    dictionary['response'] = 'invalid email address'
+                    return make_response(jsonify(
+                        {"message": "invalid email address"}), 400)
                 else:
                     user = User(kwargs['name'], kwargs['username'], kwargs['email'],
                                 kwargs['password_hash'])
                     db.session.add(user)
                     db.session.commit()
-                    dictionary['status_code'] = 201
-                    dictionary['response'] = 'User successfully created.'
+                    return make_response(jsonify(
+                        {"message": "User successfully created."}), 201)
+
         except Exception as e:
             print(str(e))
-            response = ''
             if (str(e).find('Key (email)')) > 0 or str(e).find('UNIQUE constraint failed: user.email') > 0:
-                response = 'email already exists.'
-
+                return make_response(jsonify(
+                    {"message": "email already exists."}), 201)
             elif (str(e).find('Key (username)')) > 0 or str(e).find('UNIQUE constraint failed: user.username') > 0:
-                response = 'username already exists.'
+                return make_response(jsonify(
+                    {"message": "username already exists."}), 201)
 
-            dictionary['status_code'] = 400
-            dictionary['response'] = response
-
-        return dictionary
 
     def login(self, username, password):
         ''' authenticates the user '''
